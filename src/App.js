468ed6bf129836212c20eeafa1355a0807bc8b05
farhanxovar <90099476+farhanxovar@users.noreply.github.com>
2023-05-11 22:13:05 +0600

    Boiler Code

--- src/App.js
+++ src/App.js
@@ -3,25 +3,15 @@ import "./App.css";
 
 function App() {
 
-  const [data, setData] = useState(0);
-
-  const increment = () =>{
-    setData(data + 1);
-  }
-
-  const decrement = () =>{
-    setData(data - 1);
-  }
-
   return (
     <div className="d-flex justify-content-center">
       <div class="card mt-5" style={{width: "18rem"}}>
         <div class="card-body">
-          <h5 class="card-title text-center">{data}</h5>
+          <h5 class="card-title text-center">0</h5>
             <hr/>
           <div className="mt-5">
-            <button class="btn btn-danger" onClick={decrement}>Decrement</button>
-            <button class="btn btn-primary ms-5" onClick={increment}>Increment</button>
+            <button class="btn btn-danger">Decrement</button>
+            <button class="btn btn-primary ms-5">Increment</button>
           </div>
         </div>
       </div>
